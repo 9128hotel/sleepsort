32d168eb3f23eb33f2152419bdd612d234f15173
9128hotel <9128hotel@gmail.com>
2022-12-23 11:51:16 +0000

    new file:   .gitignore new file:   fun.cpp new file:   fun.exe new file:   fun.obj

---
+++ fun.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+void generateNumList() {
+	int unsortedList[5];
+
+	for (int i = 0; i < sizeof(unsortedList); i++) {
+
+	}
+}
+
+int main() {
+	generateNumList();
+	return 0;
+};
